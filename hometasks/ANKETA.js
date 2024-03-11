54181f8709f2a4e179d397a219fa10fa992d110b
dashabeketova <kashevskayad@mail.ru>
2024-03-11 22:04:05 +0300

    dsfdsf

--- hometasks/ANKETA.js
+++ hometasks/ANKETA.js
@@ -1,20 +1,30 @@
-const name = prompt("Ваше имя?");
-const name2 = prompt("Ваша фамилия?");
-const name3 = prompt("Ваше отчество?");
-const age = parseInt(prompt("Ваш возраст?"));
+// debugger
+const name = promptName(prompt("Ваше имя?"));
+const name2 = promptName(prompt("Ваша фамилия?"));
+const name3 = promptName(prompt("Ваше отчество?"));
+
+function promptName(word) {
+  while (Number(word) || !Boolean(word)) {
+    word = prompt("Вы ввели некорректные данные");
+  }
+  return word;
+}
+let age = promptAge(parseInt(prompt("Введите возраст", 18)));
+
+function promptAge(age) {
+  while (!Boolean(age) || age <= 0 || age >= 120)
+    age = parseInt(prompt("Повторите попытку ввода возраста", 18));
+  return parseInt(age);
+}
+
 const gender = confirm("Ваш пол женский?");
+
 const age2 = age * 365;
 const future = age + 5;
 let pol;
 let old;
 
-if (gender) {
-  pol = "женский";
-} else {
-  pol = "мужской";
-}
-//через ?
-// gender ? (pol = "женский") : (pol = "мужской");
+gender ? (pol = "женский") : (pol = "мужской");
 
 if (pol == "женский" && age >= 58) {
   old = "да";
@@ -25,14 +35,6 @@ if (pol == "женский" && age >= 58) {
 } else {
   old = "нет";
 }
-//через ?
-// pol == "женский" && age >= 58
-//   ? (old = "да")
-//   : pol == "женский" && age < 58
-//   ? (old = "нет")
-//   : pol == "мужской" && age >= 63
-//   ? (old = "да")
-//   : (old = "нет");
 
 const fullName = name + name2 + name3;
 
