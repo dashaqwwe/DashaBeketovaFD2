c73f717ba9a71098eefeaa8cbab208a6c6ffbbc3
dashabeketova <kashevskayad@mail.ru>
2024-03-12 18:42:26 +0300

    delete

--- additional_tasks/A2.js
+++
@@ -1,6 +0,0 @@
-function splitNJoin(word) {
-  let str = word.split(" ").join("");
-
-  return str;
-}
-console.log(splitNJoin("@ Наша строка @"));
--- additional_tasks/B2.js
+++
@@ -1,5 +0,0 @@
-alert(rev(prompt("Введите строку")));
-function rev(v) {
-  let result = v.split("").reverse().join("");
-  return result;
-}
