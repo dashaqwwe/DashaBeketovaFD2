234f06209bedf145d096d5a08063a52f4826d008
dashabeketova <kashevskayad@mail.ru>
2024-05-21 19:06:04 +0300

    zcxcz

--- project/game.js
+++ project/game.js
@@ -1,21 +1,30 @@
 const bodyColl = document.getElementsByTagName("body");
-const body = bodyColl[0]
+const body = bodyColl[0];
 
 const container = document.createElement("div");
-container.className = 'container'
+container.className = "container";
 
-const span = document.createElement('span')
-span.textContent = 'Memory Game'
-container.appendChild(span)
+const span = document.createElement("span");
+span.textContent = "Memory Game";
+container.appendChild(span);
 
-const game = document.createElement('div')
-game.className = 'game'
-container.appendChild(game)
+const game = document.createElement("div");
+game.className = "game";
+container.appendChild(game);
 
-const reset = document.createElement('button')
-reset.className = 'reset'
-reset.textContent = 'Reset Game'
-container.appendChild(reset)
+const reset = document.createElement("button");
+reset.className = "reset";
+reset.textContent = "Reset Game";
+container.appendChild(reset);
 
-body.appendChild(container)
+body.appendChild(container);
 
+const canvas = document.getElementById("canvas");
+
+
+
+let ctx = canvas.getContext("2d");
+ctx.fillRect(10, 10, 10, 90);
+ctx.fillRect(10, 50, 90, 10);
+ctx.fillRect(90, 10, 10, 90);
+document.body.style.backgroundImage = `url(${canvas.toDataURL()}`;
