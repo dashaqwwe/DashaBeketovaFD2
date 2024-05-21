e0f6a5dc43b9ee0c0c43907c41964e18731e4a3d
dashabeketova <kashevskayad@mail.ru>
2024-05-21 21:09:16 +0300

    fffsdf

--- project/game.js
+++ project/game.js
@@ -1,30 +1,19 @@
 const bodyColl = document.getElementsByTagName("body");
 const body = bodyColl[0];
 
-const container = document.createElement("div");
-container.className = "container";
+const wrapper = document.getElementById("page-wrapper");
 
 const span = document.createElement("span");
 span.textContent = "Memory Game";
-container.appendChild(span);
+wrapper.appendChild(span);
 
 const game = document.createElement("div");
 game.className = "game";
-container.appendChild(game);
+wrapper.appendChild(game);
 
 const reset = document.createElement("button");
-reset.className = "reset";
+reset.className = "button";
 reset.textContent = "Reset Game";
-container.appendChild(reset);
+wrapper.appendChild(reset);
 
-body.appendChild(container);
-
-const canvas = document.getElementById("canvas");
-
-
-
-let ctx = canvas.getContext("2d");
-ctx.fillRect(10, 10, 10, 90);
-ctx.fillRect(10, 50, 90, 10);
-ctx.fillRect(90, 10, 10, 90);
-document.body.style.backgroundImage = `url(${canvas.toDataURL()}`;
+body.appendChild(wrapper);
