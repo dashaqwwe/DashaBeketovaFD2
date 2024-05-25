d53365963ad82d705241141255fc9c638546d7ad
dashabeketova <kashevskayad@mail.ru>
2024-05-25 19:09:33 +0300

    dsfsdfsdf

--- project/game.js
+++ project/game.js
@@ -1,64 +1,3 @@
-
-
-const bodyColl = document.getElementsByTagName("body");
-const body = bodyColl[0];
-
-const wrapper = document.getElementById("page-wrapper");
-
-const span = document.createElement("span");
-span.textContent = "Memory Game";
-wrapper.appendChild(span);
-
-const game = document.createElement("div");
-game.className = "game";
-wrapper.appendChild(game);
-
-const musicBtn = document.createElement("button");
-musicBtn.className = "musicBtn";
-musicBtn.textContent = "Music on";
-wrapper.appendChild(musicBtn);
-const myaudio = document.getElementById("myaudio");
-
-musicBtn.addEventListener("click", () => {
-  if (myaudio.paused == true) {
-    myaudio.play();
-    musicBtn.textContent = "Music off";
-  } else if (myaudio.paused == false) {
-    myaudio.pause();
-    musicBtn.textContent = "Music on";
-  }
-});
-
-const levelsDiv = document.createElement("div");
-levelsDiv.className = "levels";
-wrapper.appendChild(levelsDiv);
-
-const reset = document.createElement("button");
-reset.className = "button";
-reset.textContent = "Reset Game";
-wrapper.appendChild(reset);
-
-body.appendChild(wrapper);
-
-let levels = {
-  1: { lvl: "Level_1", comment: "Level 1" },
-  2: { lvl: "Level_2", comment: "Level 2" },
-};
-
-let levelStr = "";
-
-for (let id in levels) {
-  let lvl = levels[id];
-  levelStr +=
-    '<input type=button value="' +
-    lvl.comment +
-    '" onclick="switchToLevelPage(' +
-    id +
-    ')"> ';
-}
-
-document.querySelector(".levels").innerHTML = levelStr;
-
 let pictures = [
   "images/img-1.png",
   "images/img-1.png",
@@ -78,7 +17,160 @@ let pictures = [
   "images/img-8.png",
 ];
 
-// let sortPictures = pictures.sort(() => (Math.random() > 0.5 ? 2 : -1));
+let sortPictures = pictures.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+window.onhashchange = switchToStateFromURLHash;
+
+const wrapper = document.getElementById("page-wrapper");
+// const game = document.querySelector(".game");
+
+let spaState = {};
+
+function switchToStateFromURLHash() {
+  const URLHash = window.location.hash;
+  const stateStr = URLHash.substring(1);
+  if (stateStr != "") {
+    let parts = stateStr.split("_");
+    spaState = { pagename: parts[0] };
+  } else spaState = { pagename: "Main" };
+
+  let pageHTML = "";
+  switch (spaState.pagename) {
+    case "Settings":
+      pageHTML += renderSettingPage();
+      break;
+    case "Main":
+      pageHTML += renderMainPage();
+      break;
+    case "Game":
+      pageHTML += renderGamePage();
+      break;
+    case "1":
+      let div = document.createElement("div");
+      div.classList.add("game");
+      sortPictures.forEach((i) => {
+        let img = document.createElement("img");
+        let box = document.createElement("div");
+        box.className = "item";
+        img.setAttribute("src", i);
+        img.draggable = false;
+        box.appendChild(img);
+        div.appendChild(box);
+
+        box.onclick = function () {
+          console.log('------------');
+          box.classList.add("boxOpen");
+          setTimeout(function () {
+            try {
+              if (document.querySelectorAll(".boxOpen").length - 1) {
+                if (
+                  document.querySelectorAll(".boxOpen")[0].innerHTML ==
+                  document.querySelectorAll(".boxOpen")[1].innerHTML
+                ) {
+                  document
+                    .querySelectorAll(".boxOpen")[0]
+                    .classList.add("boxMatch");
+                  document
+                    .querySelectorAll(".boxOpen")[1]
+                    .classList.add("boxMatch");
+                  document
+                    .querySelectorAll(".boxOpen")[1]
+                    .classList.remove("boxOpen");
+                  document
+                    .querySelectorAll(".boxOpen")[0]
+                    .classList.remove("boxOpen");
+                  if (
+                    document.querySelectorAll(".boxMatch").length ==
+                    pictures.length
+                  ) {
+                    alert("win");
+                  }
+                } else {
+                  document
+                    .querySelectorAll(".boxOpen")[1]
+                    .classList.remove("boxOpen");
+                  document
+                    .querySelectorAll(".boxOpen")[0]
+                    .classList.remove("boxOpen");
+                }
+              }
+            } catch (e) {
+              console.log(e);
+            }
+          }, 500);
+        };
+      });
+      let game = div.outerHTML;
+      pageHTML += `${game} ${renderLvlPage(1)}`;
+      break;
+    case "2":
+      pageHTML += renderLvlPage(2);
+      break;
+  }
+  wrapper.innerHTML = pageHTML;
+}
+
+function switchToState(newState) {
+  const stateStr = newState.pagename;
+  location.hash = stateStr;
+}
+
+function switchToMainPage() {
+  switchToState({ pagename: "Main" });
+}
+
+function renderMainPage() {
+  return ` <span>Memory Game</span>
+  <button onclick='switchToGamePage()'>Start Game</button>
+  <button class='settings' onclick='switchToSettingsPage()'>Settings</button>`;
+}
+
+function switchToGamePage() {
+  switchToState({ pagename: "Game" });
+}
+
+function renderGamePage() {
+  let levels = {
+    1: { lvl: "Level_1", comment: "Level 1" },
+    2: { lvl: "Level_2", comment: "Level 2" },
+  };
+  let levelStr = "";
+  let spanStr = `<span>Memory Game</span>`;
+  let backStr = `<button class='menuBtn' onclick='switchToMainPage()'>Back to menu</button>`;
+  for (let id in levels) {
+    let lvl = levels[id];
+    levelStr += `<button onclick='switchToLevelPage(${id})'>${lvl.comment}</button>`;
+  }
+  return `${spanStr + levelStr + backStr}`;
+}
+
+function switchToSettingsPage() {
+  switchToState({ pagename: "Settings" });
+}
+
+function renderSettingPage() {
+  return `<span>Memory Game</span>
+  <button class='musicBtn' onclick='switchSound()'>Music on</button>
+  <button class='menuBtn' onclick='switchToMainPage()'>Back to menu</button>`;
+}
+
+function switchToLevelPage(lvl) {
+  switchToState({ pagename: lvl });
+}
+
+function renderLvlPage(lvl) {
+  if (lvl == 1) {
+    return `
+    <button onclick='resetPage()'>Reset</button>
+    <button class='menuBtn' onclick='switchToGamePage()'>Back to levels</button>`;
+  } else {
+    return `
+    <button onclick='resetPage()'>Reset</button>
+    <button class='menuBtn' onclick='switchToGamePage()'>Back to levels</button>`;
+  }
+}
+
+switchToStateFromURLHash();
 
 // sortPictures.forEach((i) => {
 //   let img = document.createElement("img");
@@ -127,6 +219,18 @@ let pictures = [
 //   });
 // });
 
-reset.addEventListener("click", () => {
+function resetPage() {
   window.location.reload();
-});
+}
+
+const myaudio = document.getElementById("myaudio");
+function switchSound() {
+  const musicBtn = document.querySelector(".musicBtn");
+  if (myaudio.paused == true) {
+    myaudio.play();
+    musicBtn.textContent = "Music off";
+  } else if (myaudio.paused == false) {
+    myaudio.pause();
+    musicBtn.textContent = "Music on";
+  }
+}
